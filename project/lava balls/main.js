985c063985d5063d69c286c38fa4365c0162e5ef
rimdagic <aagic1@hotmail.com>
2022-10-18 19:13:52 +0200

    added lava balls

---
+++ project/lava balls/main.js
@@ -0,0 +1,5 @@
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('2d');
+
+context.fillStyle = 'rgb(255, 255, 255)'
+context.fillRect(canvas.width / 2 - 10, canvas.height / 2 - 10, 40, 40);
