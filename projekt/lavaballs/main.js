335a77a6f77c308496805d62cccc2e3783e584cb
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2022-10-18 19:00:48 +0200

    Delete projekt directory

--- projekt/lavaballs/main.js
+++
@@ -1,7 +0,0 @@
-let canvas = document.getElementById('canvas');
-let context = canvas.getContext('2d');
-
-context.beginPath();
-context.strokeStyle = 'rgb(255, 255, 255)'
-context.rect(20, 20, 150, 100);
-context.stroke();
